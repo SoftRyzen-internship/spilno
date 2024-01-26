af676c4760e91a270e4ecff742a35078343536b4
Oleksandr P <goodwin009@gmail.com>
2024-01-26 19:12:14 +0200

    rename portal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
         <Header />
         <main>{children}</main>
         <Footer />
-        <div id="portal" />
+        <div id="modal-portal" />
       </body>
     </html>
   );
