08dc320b5673533ee31b4f56169e2d600847b078
Denys Borysiuk <denisborisiuk@gmail.com>
2024-01-26 09:52:38 +0200

    add tw config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,51 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/layout/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/sections/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      xl: '1280px',
+
+      smOnly: { max: '767.98px' },
+      mdOnly: { min: '768px', max: '1279.98px' },
+    },
+
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '16px',
+        sm: '16px',
+        md: '42px',
+        xl: '32px',
+      },
+    },
+    extend: {
+      fontFamily: {
+        raleway: ['var(--font-raleway)'],
+        geologica: ['var(--font-geologica)'],
+      },
+
+      colors: {
+        white: '#FFFFFF',
+        accent: '#0052F2',
+        red: '#FF0000',
+        darkBg: '#0D0A19',
+        lightBg: '#F1F5F9',
+        headline: '#102141',
+        primaryText: '#33343C',
+        secondaryText: 'rgba(51, 52, 60, 0.70)',
+        greyText: '#8E8E93',
+        darkBlue: '#0045CB',
+        lightBlue: '#ACC8FF',
+        strokeColor: '#CBD8E5',
+      },
+    },
   },
   plugins: [],
 };
