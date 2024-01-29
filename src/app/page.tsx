28f43a54accec4a4569e167eb9209230ad6a31b7
Dmitry Abakumov <dm.abakumov@gmail.com>
2024-01-29 11:09:09 +0200

    Adds SectionTitle componenent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import { SectionTitle } from '@/components/ui/SectionTitle';
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="container">
+        <SectionTitle
+        // Стили, для того, чтобы проверить отбражение в секции About
+        // className="text-[18px]/[1.3] md:text-[20px]/[1.3] xl:text-[34px]/[1.3]"
+        >
+          Хочеш розширити свій бізнес у віртуальному просторі?
+        </SectionTitle>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/ui/SectionTitle/types.ts
@@ -0,0 +1,8 @@
+import { ReactNode } from 'react';
+
+export type Props = {
+  children: ReactNode;
+  className?: string;
+  isWhite?: boolean;
+  isCentered?: boolean;
+};
