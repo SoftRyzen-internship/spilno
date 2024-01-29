d335d0dfc0d51e6aef666172e075574e7a296946
Denys Borysiuk <denisborisiuk@gmail.com>
2024-01-29 13:25:49 +0200

    add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { SectionTitle } from '@/components/ui/SectionTitle';
 import { ModalTest } from '@/components/ui/ModalTest';
+import { Logo } from '@/components/ui/Logo';
 
 export default function Home() {
   return (
     <div>
+      <div className=" container bg-darkBg">
+        <Logo location="header" />
+      </div>
       <div className="container">
         <SectionTitle
         // Стили, для того, чтобы проверить отбражение в секции About
