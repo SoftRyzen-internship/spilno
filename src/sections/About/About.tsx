5696936604f0d6359442beb0d170063fb112a6b4
Dmitry Abakumov <dm.abakumov@gmail.com>
2024-02-05 16:28:50 +0200

    adds mobile and desktop styles of about section

--- src/sections/About/About.tsx
+++ src/sections/About/About.tsx
@@ -11,11 +11,14 @@ import data from '@/data/about.json';
 export const About = () => {
   const { title, ourGoal, orderBtnText } = data;
   return (
-    <section className="overflow-hidden bg-darkBg py-[60px]" id="about">
-      <div className="container relative flex flex-col items-center text-center md:items-start">
+    <section
+      className="overflow-hidden bg-darkBg py-[60px] md:py-[80px] xl:py-[178px]"
+      id="about"
+    >
+      <div className="container relative flex flex-col items-center text-center md:items-start md:text-start">
         <RingedImage
           variant="right"
-          className="mb-8 w-[219px] translate-x-[-25%] md:absolute md:right-[-89px] md:top-[54px] md:w-[259px] xl:w-[537px]"
+          className="mb-8 w-[219px] translate-x-[-25%] md:absolute md:right-[-24px] md:top-[54px] md:w-[259px] xl:right-[-195px] xl:top-[-58px] xl:w-[537px]"
         >
           <Image
             alt="about"
@@ -25,13 +28,18 @@ export const About = () => {
             sizes="(max-width: 479px) 100vw,(min-width: 480px) 270px, (min-width: 768px) 259px, (min-width: 1280px) 537px"
           />
         </RingedImage>
-        <div className="md:w-[330px]">
-          <SectionTitle className="mb-4" isWhite>
+        <div className="md:w-[330px] xl:w-[588px]">
+          <SectionTitle
+            className="mb-4 text-[18px]/[1.3] md:text-[20px] xl:mb-6 xl:text-[34px]"
+            isWhite
+          >
             {title}
           </SectionTitle>
-          <p className="mb-12 text-[14px]/[1.5] text-white">{ourGoal}</p>
+          <p className="mb-12 text-[14px]/[1.5] text-white md:text-[16px] xl:text-[20px]">
+            {ourGoal}
+          </p>
           <Button
-            className="w-full md:w-[186px]"
+            className="w-full md:w-auto"
             text={orderBtnText}
             link="#faq"
           />
