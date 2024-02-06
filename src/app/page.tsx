149e287498b4e54aa42d361ca6fee67ad8fa2b83
Denys Borysiuk <denisborisiuk@gmail.com>
2024-02-06 10:42:26 +0200

    clear page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,32 +5,23 @@ import { About } from '@/sections/About';
 import { Offer } from '@/sections/Offer';
 import { Reviews } from '@/sections/Reviews';
 import { FAQ } from '@/sections/FAQ';
-import { CaseCard } from '@/components/ui/CaseCard';
-
-import data from '@/data/cases.json';
 
 export default function Home() {
   return (
     <>
       <Hero />
 
-      <ul className="container my-[50px] flex flex-col gap-6">
-        {data.caseCards.map(card => (
-          <CaseCard key={card.id} {...card} />
-        ))}
-      </ul>
+      <Offer />
 
       <Advantages />
 
-      <ContactUs />
-
       <About />
-
-      <Offer />
 
       <Reviews />
 
       <FAQ />
+
+      <ContactUs />
     </>
   );
 }
