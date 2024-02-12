14ace977a432078f408aede76285b94e70d98849
Dmitry Abakumov <dm.abakumov@gmail.com>
2024-02-12 17:58:43 +0200

    translates telegram fields to Ukrainian

--- src/actions/telegram/sendTelegramData.ts
+++ src/actions/telegram/sendTelegramData.ts
@@ -1,15 +1,19 @@
+import data from '@/data/common.json';
+
 import { TelegramData, TelegramRes } from '@/types';
 
 const ENTRY = '<b>Замовлення проєкту:</b>\n';
 
-export const sendTelegramData = async (data: TelegramData) => {
-  try {
-    const formFields = Object.keys(data);
-    const telegramMessage = formFields.reduce(
-      (acc, field) => (acc += `${field}: ${data[field]}\n`),
-      ENTRY,
-    );
+export const sendTelegramData = async (formData: TelegramData) => {
+  const { telegramFieldNames } = data;
 
+  const customerData = Object.values(formData);
+  const telegramMessage = telegramFieldNames.reduce(
+    (acc, field, idx) => (acc += `${field}: ${customerData[idx]}\n`),
+    ENTRY,
+  );
+
+  try {
     const res: TelegramRes = await fetch('/api/telegram', {
       method: 'POST',
       mode: 'no-cors',
