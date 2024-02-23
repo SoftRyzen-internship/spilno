a07739d1c6d42d61411e73a444a61f3e74b18932
Oleksandr P <goodwin009@gmail.com>
2024-02-23 11:55:33 +0200

    add tablet and desktop styles

--- src/components/ui/OverviewStage/OverviewStage.tsx
+++ src/components/ui/OverviewStage/OverviewStage.tsx
@@ -12,33 +12,43 @@ export const OverviewStage: React.FC<OverviewStageProps> = ({
 }) => {
   return (
     <div className={containerStyle}>
-      <h3 className="font-geologica text-2xl text-headline smOnly:mb-6">
+      <h3
+        className={cn(
+          'font-geologica text-xl/[1.35] text-headline md:w-[212px] md:shrink-0 md:text-2xl/[1.35] xl:w-[485px] xl:text-[42px]/[1.35] smOnly:mb-6',
+          { 'smOnly:mb-4': title === 'Рішення' },
+          { 'mb-4 md:mx-auto md:text-center xl:mb-6': title === 'Результат' },
+        )}
+      >
         {title}
       </h3>
 
       <p
-        className={cn('text-sm/[1.5] text-primaryText', {
-          'smOnly:mb-8': keySolutions,
-        })}
+        className={cn(
+          'text-sm/[1.5] text-primaryText md:text-base xl:text-2xl/[1.5]',
+          {
+            'max-w-[296px] md:order-last md:text-left md:text-sm/[1.5] xl:max-w-[493px] xl:xl:text-xl/[1.5] smOnly:mb-8':
+              title === 'Рішення',
+          },
+        )}
       >
         {desc}
       </p>
 
       {keySolutions && (
-        <ul className="space-y-6">
+        <ul className="space-y-6 md:row-span-3 xl:space-y-10">
           {keySolutions.map(keySolution => (
-            <li key={keySolution.label} className="relative pl-[34px]">
+            <li key={keySolution.label} className="relative pl-[34px] xl:pl-14">
               <CheckIcon
                 width={20}
                 heigth={20}
-                className="absolute left-0 top-0 size-5 text-accent"
+                className="absolute left-0 top-0 size-5 text-accent md:size-6 xl:size-8"
               />
 
-              <h4 className="mb-2 font-geologica text-base/[1.25] text-headline">
+              <h4 className="mb-2 font-geologica text-base/[1.25] text-headline md:text-lg/[1.25] xl:text-2xl/[1.25]">
                 {keySolution.label}
               </h4>
 
-              <p className="text-sm/[1.5] text-primaryText">
+              <p className="text-sm/[1.5] text-primaryText xl:text-xl/[1.5]">
                 {keySolution.summary}
               </p>
             </li>
--- src/sections/Overview/Overview.tsx
+++ src/sections/Overview/Overview.tsx
@@ -2,12 +2,9 @@ import Image from 'next/image';
 
 import { Button, OverviewStage } from '@/components/ui';
 
-import { cn } from '@/utils/cn';
 import data from '@/data/overview.json';
 
 import IconArrow from '~/icons/arrow.svg';
-
-import css from './Overview.module.css';
 
 export const Overview: React.FC = async () => {
   const {
@@ -24,53 +21,56 @@ export const Overview: React.FC = async () => {
   return (
     <section
       id={id}
-      className={cn('relative bg-white pb-[60px] md:pb-20', css.dynamicPadding)}
+      className="relative bg-white pb-[60px] after:absolute after:bottom-0 after:left-1/2 after:h-[1px] after:w-screen after:-translate-x-1/2 after:bg-strokeColor after:content-[''] md:pb-20"
     >
+      <Image
+        src={primaryImage.src}
+        alt={primaryImage.alt}
+        width={1400}
+        height={700}
+        priority
+        sizes="(max-width: 1399px) 100vw, (min-width: 1400px) 1400px"
+        className="mx-auto aspect-[1.64] w-full object-cover md:aspect-[2.06] xl:aspect-[2/1] xl:max-w-[1400px]"
+      />
+
       <div className="container md:space-y-20 smOnly:space-y-[60px]">
         <h2 className="hidden">{title}</h2>
-
-        <Image
-          src={primaryImage.src}
-          alt={primaryImage.alt}
-          width={1400}
-          height={700}
-          priority
-          sizes="(max-width: 1399px) 100vw, (min-width: 1400px) 1400px"
-          className="absolute left-1/2 top-0 aspect-[1.64] -translate-x-1/2 object-cover md:aspect-[2/1] xl:max-w-[1400px] smOnly:!m-0 smOnly:w-full"
-        />
 
         <OverviewStage
           title={problem.title}
           desc={problem.desc}
-          containerStyle="text-left smOnly:!m-0 md:flex"
+          containerStyle="text-left md:flex md:gap-[10px] xl:gap-8"
         />
 
-        <div className="">
-          <Image
-            src={problem.wideImage.src}
-            alt={problem.wideImage.alt}
-            width={677}
-            height={500}
-            loading="lazy"
-            className="rounded-[10px] object-cover smOnly:mb-4 smOnly:w-full"
-            sizes="(max-width: 479px) 100vw,(min-width: 480px) 448px, (min-width: 768px) 378px, (min-width: 1280px) 500px"
-          />
-
-          <Image
-            src={problem.squareImage.src}
-            alt={problem.squareImage.alt}
-            width={500}
-            height={500}
-            loading="lazy"
-            className="aspect-square rounded-[10px] object-cover smOnly:w-full"
-            sizes="(max-width: 479px) 100vw,(min-width: 480px) 448px, (min-width: 768px) 378px, (min-width: 1280px) 500px"
-          />
-        </div>
+        <ul className="md:flex md:h-[278px] md:gap-6 xl:h-[500px] xl:gap-10 smOnly:space-y-4">
+          <li>
+            <Image
+              src={problem.wideImage.src}
+              alt={problem.wideImage.alt}
+              width={677}
+              height={500}
+              loading="lazy"
+              className="rounded-[10px] object-cover md:h-full smOnly:w-full"
+              sizes="(max-width: 479px) 100vw,(min-width: 480px) 448px, (min-width: 768px) 382px, (min-width: 1280px) 677px"
+            />
+          </li>
+          <li>
+            <Image
+              src={problem.squareImage.src}
+              alt={problem.squareImage.alt}
+              width={500}
+              height={500}
+              loading="lazy"
+              className="rounded-[10px] object-cover md:h-full smOnly:w-full"
+              sizes="(max-width: 479px) 100vw,(min-width: 480px) 448px, (min-width: 768px) 278px, (min-width: 1280px) 500px"
+            />
+          </li>
+        </ul>
 
         <OverviewStage
           title={analysis.title}
           desc={analysis.desc}
-          containerStyle="text-left"
+          containerStyle="text-left md:flex md:gap-[10px]"
         />
 
         <Image
@@ -79,60 +79,66 @@ export const Overview: React.FC = async () => {
           width={1216}
           height={550}
           loading="lazy"
-          className="h-[210px] rounded-[10px] object-cover smOnly:w-full"
-          sizes="(max-width: 479px) 100vw,(min-width: 480px) 448px, (min-width: 768px) 378px, (min-width: 1280px) 500px"
+          className="w-full rounded-[10px] object-cover smOnly:h-[210px]"
+          sizes="(max-width: 479px) 100vw,(min-width: 480px) 448px, (min-width: 768px) 684px, (min-width: 1280px) 1216px"
         />
 
         <OverviewStage
           title={decision.title}
           desc={decision.desc}
           keySolutions={decision.keySolutions}
-          containerStyle="text-left"
+          containerStyle="text-left md:grid md:gap-4 md:grid-cols-2 xl:gap-6"
         />
 
-        <div className="smOnly:space-y-4">
-          <Image
-            src={conclusion.overviewImg.src}
-            alt={conclusion.overviewImg.alt}
-            width={1216}
-            height={480}
-            loading="lazy"
-            className="rounded-[10px] object-cover smOnly:w-full"
-            sizes="(max-width: 479px) 100vw,(min-width: 480px) 448px, (min-width: 768px) 378px, (min-width: 1280px) 500px"
-          />
+        <ul className="md:flex md:flex-wrap md:justify-between md:gap-y-6 xl:gap-y-10 smOnly:space-y-4">
+          <li className="md:basis-full">
+            <Image
+              src={conclusion.overviewImg.src}
+              alt={conclusion.overviewImg.alt}
+              width={1216}
+              height={480}
+              loading="lazy"
+              className="w-full rounded-[10px] object-cover xl:h-[480px] smOnly:h-[169px]"
+              sizes="(max-width: 479px) 100vw,(min-width: 480px) 448px, (min-width: 768px) 684px, (min-width: 1280px) 1216px"
+            />
+          </li>
 
-          <Image
-            src={conclusion.squareImg.src}
-            alt={conclusion.squareImg.alt}
-            width={500}
-            height={500}
-            loading="lazy"
-            className="aspect-square rounded-[10px] object-cover smOnly:w-full"
-            sizes="(max-width: 479px) 100vw,(min-width: 480px) 448px, (min-width: 768px) 378px, (min-width: 1280px) 500px"
-          />
+          <li className="md:h-[282px] xl:h-[500px]">
+            <Image
+              src={conclusion.squareImg.src}
+              alt={conclusion.squareImg.alt}
+              width={500}
+              height={500}
+              loading="lazy"
+              className="w-full rounded-[10px] object-cover md:h-full"
+              sizes="(max-width: 479px) 100vw,(min-width: 480px) 448px, (min-width: 768px) 282px, (min-width: 1280px) 500px"
+            />
+          </li>
 
-          <Image
-            src={conclusion.wideImg.src}
-            alt={conclusion.wideImg.alt}
-            width={677}
-            height={500}
-            loading="lazy"
-            className="rounded-[10px] object-cover smOnly:w-full"
-            sizes="(max-width: 479px) 100vw,(min-width: 480px) 448px, (min-width: 768px) 378px, (min-width: 1280px) 500px"
-          />
-        </div>
+          <li className="md:h-[282px] xl:h-[500px]">
+            <Image
+              src={conclusion.wideImg.src}
+              alt={conclusion.wideImg.alt}
+              width={677}
+              height={500}
+              loading="lazy"
+              className="w-full rounded-[10px] object-cover md:h-full"
+              sizes="(max-width: 479px) 100vw,(min-width: 480px) 448px, (min-width: 768px) 378px, (min-width: 1280px) 677px"
+            />
+          </li>
+        </ul>
 
         <div>
           <OverviewStage
             title={conclusion.title}
             desc={conclusion.desc}
-            containerStyle="text-center mb-8"
+            containerStyle="text-center mb-8 md:max-w-[538px] md:mx-auto md:mb-10 xl:max-w-[804px] xl:mb-12"
           />
 
           <Button
             text={button.label}
             btnStyle="accent"
-            className="max-md:flex w-full md:max-w-[274px] xl:max-w-[338px]"
+            className="flex w-full md:mx-auto md:max-w-[223px] xl:max-w-[338px]"
             link={button.target}
           >
             <IconArrow className="size-5 xl:size-6" aria-hidden="true" />
